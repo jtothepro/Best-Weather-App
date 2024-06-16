5b38dcad579f22076cf8043f1cb8c1d7d7e530f9
jtothepro <168333109+jtothepro@users.noreply.github.com>
2024-06-16 15:24:47 +0200

    Added function to change the temperature - with a bug

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
+function updatedData(response) {
+  let temperatureElement = document.querySelector("#temperature-element");
+  temperatureElement.innerHTML = response.data.temperature.current;
+  console.log(temperatureElement);
+}
+
+function citySearchedWeather(city) {
+  let apiKey = `85be9c7bad2eb4fafc3fe0e35t2o0c3e`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(updatedData);
+}
+
 function searchSubmitForm(event) {
   event.preventDefault();
-  {
-    let searchFieldInput = document.querySelector("#search-field");
-    let cityName = document.querySelector("#weather-city");
-    cityName.innerHTML = searchFieldInput.value;
-  }
+
+  let searchFieldInput = document.querySelector("#search-field");
+  let cityName = document.querySelector("#weather-city");
+  cityName.innerHTML = searchFieldInput.value;
+  citySearchedWeather(searchFieldInput.value);
 }
 
 let searchForm = document.querySelector("#search-form");
