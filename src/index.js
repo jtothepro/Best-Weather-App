9d99cf8ecea83b5af9af7e326e513b6e622b1a33
jtothepro <168333109+jtothepro@users.noreply.github.com>
2024-06-18 16:40:08 +0200

    Added a default city when refresh page

--- src/index.js
+++ src/index.js
@@ -22,3 +22,5 @@ function searchSubmitForm(event) {
 
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", searchSubmitForm);
+
+citySearchedWeather("Stuttgart");
